bdf23c8533be6a9aacf3ed485f345372cd92f6aa
Andrew Ackah-Miezan <tawala@gmail.com>
2017-01-10 00:48:15 +0100

    input pour send message directive pour gerer events methods pour gerer events styles footer sendmessage 2 db method newmesage

--- client/scripts/controllers/chat.controller.js
+++ client/scripts/controllers/chat.controller.js
@@ -1,3 +1,6 @@
+import Ionic from 'ionic-scripts';
+import { _ } from 'meteor/underscore';
+import { Meteor } from 'meteor/meteor';
 import { Controller } from 'angular-ecmascript/module-helpers';
 import { Chats, Messages } from '../../../lib/collections';
  
@@ -6,7 +9,9 @@ export default class ChatCtrl extends Controller {
     super(...arguments);
  
     this.chatId = this.$stateParams.chatId;
- 
+    this.isIOS = Ionic.Platform.isWebView() && Ionic.Platform.isIOS();
+    this.isCordova = Meteor.isCordova;
+
     this.helpers({
         messages(){
             return Messages.find({ chatId: this.chatId })
@@ -16,7 +21,47 @@ export default class ChatCtrl extends Controller {
         }
     });
   }
+
+  sendMessage() {
+      if (_.isEmpty(this.message)) return;
+ 
+    this.callMethod('newMessage', {
+      text: this.message,
+      type: 'text',
+      chatId: this.chatId
+    });
+ 
+    delete this.message;
+  }
+
+  inputUp () {
+    if (this.isIOS) {
+      this.keyboardHeight = 216;
+    }
+ 
+    this.scrollBottom(true);
+  }
+ 
+  inputDown () {
+    if (this.isIOS) {
+      this.keyboardHeight = 0;
+    }
+ 
+    this.$ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+ 
+  closeKeyboard () {
+    if (this.isCordova) {
+      cordova.plugins.Keyboard.close();
+    }
+  }
+ 
+  scrollBottom(animate) {
+    this.$timeout(() => {
+      this.$ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(animate);
+    }, 300);
+  }
 }
  
 ChatCtrl.$name = 'ChatCtrl';
-ChatCtrl.$inject = ['$stateParams'];
+ChatCtrl.$inject = ['$stateParams', '$timeout', '$ionicScrollDelegate'];
